b9ebd44d82ae010f9c37d54595e5df2ba67bbdaa
Yatheesh Nagella <yatheeshnagella17@gmail.com>
2025-05-24 19:20:19 -0500

    Enable Sanctum in  Laravel App

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Api\LoginController;
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/login', [LoginController::class, 'login']);
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
